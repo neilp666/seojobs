72f63d172a991914b48e557c5cf097297d0122a6
neil patel <neil@myindianweddingapp.com>
2016-04-04 15:03:04 +0100

    Configure AWS & paperclip and tested

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'devise'
 gem 'cancan'
 gem "paperclip", git: "git://github.com/thoughtbot/paperclip.git"
 gem 'dotenv-rails', groups: [:development]
-
+gem 'aws-sdk', '~> 2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,18 @@ module Job1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
+    config.paperclip_defaults = { 
+        storage: :s3,
+        s3_region: 'us-west-2',
+        s3_credentials:{
+            bucket: ENV['AWS_BUCKET'],
+            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+        }
+    }
   end
 end
